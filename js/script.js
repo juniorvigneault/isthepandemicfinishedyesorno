1165d3607a6c5ae1ca130e42f8ea73646b116619
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2021-01-14 14:35:03 -0500

    isthepandemicfinishedyesorno: finished the simulation

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
 "use strict";
 
-// exrecise by just junior
+// A page to check if the global pandemic is still going on
+// muzak by : bensound.com
+// design and code by Junior
 
-let spin = 0;
 
-let titleString = `is the pandemic finished yes or no`;
+// variables for the program
+
+let titlePNG;
 let titleStringx = undefined;
 let titleStringy = undefined;
 
-let string = "No, sorry";
+
 let stringx = undefined;
 let stringy = undefined;
-
-let string2 = "Try again later";
-
-let string3 = "*";
-
-let asteriksx;
-let asteriksy;
 
 let state = `title`;
 
@@ -27,10 +23,6 @@ let letsfindout1y;
 
 let letsfindout2;
 
-let letsfindout2x;
-let letsfindout2y;
-let letsfindout2Appear = false;
-
 let arrows;
 
 let sablier;
@@ -39,10 +31,18 @@ let cursor;
 
 let click;
 let muzak;
+let printerSFX;
 
 let noSorrygif;
+let noSorrygifx = 3000;
+let noSorrygify = 3000;
 
-function preload(){
+let didUKnow;
+
+// preloading the media
+function preload() {
+  titlePNG = loadImage(`assets/images/title.png`);
+
   letsfindout1 = loadImage(`assets/images/letsfindout1.png`);
   letsfindout2 = loadImage(`assets/images/letsfindout2.png`);
 
@@ -56,7 +56,12 @@ function preload(){
 
   muzak = loadSound(`assets/sounds/muzak.mp3`);
 
-  noSorrygif = loadImage(`assets/images/nosorry.gif`)
+  noSorrygif = loadImage(`assets/images/sorry.gif`);
+
+  printerSFX = loadSound(`assets/sounds/printer.mp3`)
+
+  didUKnow = loadImage(`assets/images/diduknow.png`);
+
 }
 
 function setup() {
@@ -65,70 +70,153 @@ function setup() {
 }
 
 
-function draw(){
+function draw() {
   background(0);
+  // rectangle greyish background for all the states
   backgroundColor();
-  // moveBackground();
-  // circlesBackground();
-  // noSorry();
-  // tryAgain();
-  // asteriks();
+  // switching between states
   stateSwitch();
+  // pixelated white hand cursor
   handCursor();
 }
 
+// resizes the canvas so that things appear when making bigger
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function stateSwitch(){
-  if (state === `title` ) {
+// function that calls the 3 different states
+function stateSwitch() {
+  if (state === `title`) {
     title();
-  }
-  else if (state === `loading`){
+  } else if (state === `loading`) {
     loading();
-  }
-  else if (state === `noSorry`){
+  } else if (state === `noSorry`) {
     noSorry();
   }
 }
 
-// create 1 line of circles
-function circles(x, y) {
-  for (let i = 0; i < 20; i++) {
-    ellipseMode(CENTER);
-    noStroke();
-
-    fillHsluv(100, 20, 60);
-    ellipse(x, y, 100);
-    x = x + 150;
-  }
-}
-
-function backgroundColor(){
+// grey background
+function backgroundColor() {
   // background rect
   push();
   noStroke();
-  fillHsluv(0,20, 90);
-  rect(0,0, windowWidth, windowHeight);
+  fillHsluv(0, 20, 90);
+  rect(0, 0, windowWidth, windowHeight);
   pop();
 }
 
-// // create a pattern with lines of circles
-function circlesBackground() {
-    let x = - 1500;
-    let y = - 1000;
-    for (let i = 0; i < 20; i++) {
-      circles(x, y);
-      y = y + 150;
-    }
+// ending state with answer
+function noSorry() {
+  push()
+  imageMode(CENTER);
+  // gif with the answer that prints out
+  image(noSorrygif, noSorrygifx, noSorrygify);
+  pop();
+  // for some reason my gif was showing the last frame for a second before starting
+  // so I had to make it dissapear far away for that second and then back where
+  // I wanted it to be shown
+  setTimeout(noSorryFix, 500);
+
 }
 
-// function moveBackground() {
-//     spin = spin + .07;
-//     translate(width/2, height/2);
-//     rotate(radians(spin))
-// }
+// title state with question and button to ask
+function title() {
+
+  titleStringx = width / 2;
+  titleStringy = height * .3 - 100;
+
+// display the different buttons and arrow
+  displayButtons();
+  push();
+  imageMode(CENTER);
+  image(titlePNG, titleStringx, titleStringy);
+  pop();
+}
+
+// display the different buttons and arrow
+function displayButtons() {
+
+  letsfindout1x = width / 2;
+  letsfindout1y = titleStringy + 450;
+
+
+  push();
+  imageMode(CENTER);
+  image(arrows, width / 2, titleStringy + 250);
+  pop();
+
+  // make the button switch when hovering over it
+  if (mouseX > letsfindout1x - letsfindout1.width / 2 &&
+    mouseX < letsfindout1x + letsfindout1.width / 2 &&
+    mouseY > letsfindout1y - letsfindout1.height / 2 &&
+    mouseY < letsfindout1y + letsfindout1.height / 2) {
+    push();
+    imageMode(CENTER);
+    image(letsfindout2, letsfindout1x, letsfindout1y)
+    pop();
+  }
+
+  else {
+    push();
+    imageMode(CENTER);
+    image(letsfindout1, letsfindout1x, letsfindout1y)
+    pop();
+  }
+
+}
+
+// pressing the button brings us to the second state of the program
+function mousePressed() {
+  if (mouseX > letsfindout1x - letsfindout1.width / 2 &&
+    mouseX < letsfindout1x + letsfindout1.width / 2 &&
+    mouseY > letsfindout1y - letsfindout1.height / 2 &&
+    mouseY < letsfindout1y + letsfindout1.height / 2) {
+    state = `loading`;
+    click.play();
+    muzak.loop();
+    setTimeout(playPrinter, 20400);
+  }
+}
+
+// loading state, where you wait for the answer if the pandemic is indeed finished
+function loading() {
+  push();
+  imageMode(CENTER);
+  image(didUKnow, width / 2, height / 2);
+  pop();
+  push();
+  imageMode(CENTER);
+  image(sablier, width / 2, height / 2);
+  pop();
+  setTimeout(function() {
+    state = 'noSorry';
+  }, 20400);
+
+  letsfindout1x = 3000;
+  letsfindout1y = 3000;
+
+}
+
+// white hand cursor
+function handCursor() {
+  push();
+  imageMode(CENTER);
+  image(cursor, mouseX, mouseY);
+  pop();
+}
+
+// gif fix that shows the last frame. After appearing at (3000,3000) it appears back in the middle
+function noSorryFix() {
+  noSorrygifx = width / 2;
+  noSorrygify = height / 2;
+}
+
+// bleak sound of printer while answer shows up
+function playPrinter() {
+  printerSFX.play();
+  muzak.stop();
+}
 
 // functions to translate RGB to HSLuv for fill and stroke
 function fillHsluv(h, s, l) {
@@ -139,90 +227,4 @@ function fillHsluv(h, s, l) {
 function strokeHsluv(h, s, l) {
   const rgb = hsluv.hsluvToRgb([h, s, l]);
   stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
-}
-
-function noSorry(){
-  push()
-  imageMode(CENTER);
-  image(noSorrygif, width/2, height/2);
-  pop();
-}
-
-function tryAgain(){
-  push()
-  textAlign(CENTER, CENTER);
-  textSize(20);
-  // textStyle(BOLD);
-  fillHsluv(10,90,50);
-
-  text(string2, width/2, stringy + 100);
-  pop();
-}
-
-function asteriks(){
-  push();
-  textAlign(CENTER, CENTER);
-  textSize(90);
-  fillHsluv(10,90,50);
-  asteriksx = stringx - 115;
-  asteriksy = stringy - 30;
-  text(string3, asteriksx, asteriksy);
-  pop();
-}
-
-function title(){
-
-  // text
-  push();
-  textAlign(CENTER, CENTER);
-  textSize(30);
-  // textStyle(BOLD);
-  fillHsluv(0,0,0);
-
-  titleStringx = width/2;
-  titleStringy = height * .3;
-
-  text(titleString, titleStringx, titleStringy);
-  displayButtons();
-
-  pop();
-
-function displayButtons(){
-    push();
-    imageMode(CENTER);
-
-    letsfindout1x = width/2;
-    letsfindout1y = titleStringy + 300;
-
-    image(letsfindout1, letsfindout1x, letsfindout1y);
-    image(arrows, width/2, titleStringy + 120);
-    pop();
-  }
-}
-
-function mousePressed(){
-  if (mouseX > letsfindout1x - letsfindout1.width / 2 &&
-  mouseX < letsfindout1x + letsfindout1.width / 2 &&
-  mouseY > letsfindout1y - letsfindout1.height / 2 &&
-  mouseY < letsfindout1y + letsfindout1.height / 2) {
-    state = `loading`;
-    click.play();
-    muzak.loop();
-}
-}
-
-function loading(){
-  push();
-  imageMode(CENTER);
-  image(sablier, width/2, height/2);
-  pop();
-
-  setTimeout(function(){ state = 'noSorry'; }, 17900);
-}
-
-function handCursor(){
-  push();
-  imageMode(CENTER);
-  image(cursor, mouseX, mouseY);
-  pop();
 }
